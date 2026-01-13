654f08e04bf7f2f30b111f6e7389453591762f24
joc36395 <jochen.meidow@iosb.fraunhofer.de>
2026-01-13 11:57:08 +0100

    revision file conncomp.h

--- src/conncomp.h
+++ src/conncomp.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the GreasePad distribution (https://github.com/FraunhoferIOSB/GreasePad).
- * Copyright (c) 2022-2025 Jochen Meidow, Fraunhofer IOSB
+ * Copyright (c) 2022-2026 Jochen Meidow, Fraunhofer IOSB
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,10 +20,7 @@
 #define CONNCOMP_H
 
 #include <Eigen/Core>
-#include <Eigen/Dense>
 #include <Eigen/SparseCore>
-
-#include "matrix.h"
 
 
 //! Sparse incidence matrix and connected components
@@ -32,17 +29,17 @@ namespace Graph {
 using Eigen::SparseMatrix;
 using Eigen::ColMajor;
 using Eigen::Index;
-using Eigen::Matrix;
 using Eigen::Dynamic;
 using Eigen::VectorXi;
-using Eigen::VectorXidx;
+using Eigen::Vector;
 
 
+//! Matlab's  bins = conncomp( AA, 'outputForm', 'vector')
 [[nodiscard,maybe_unused]] static VectorXi conncomp(const SparseMatrix<int, ColMajor> &AA)
 {
 
     static VectorXi m_comp;    // index vector components, 0-based
-    static Eigen::Vector<bool,Dynamic> m_visited;    // for each vertex: visited?
+    static Vector<bool,Dynamic> m_visited;    // for each vertex: visited?
 
     struct s {
         static void depthFirstSearch( const SparseMatrix<int,ColMajor> & CC, int c, Index v)
@@ -72,29 +69,6 @@ using Eigen::VectorXidx;
     return m_comp;
 }
 
-
-/* Connected components (vector with indices/labels)
-class ConnComp
-{
-public:
-    explicit ConnComp( const SparseMatrix<int, ColMajor> & BB);  //!< Value constructor with sparse matrix
-
-    // [[nodiscard]] VectorXidx mapHead( int cc, Index n) const;  //!< Get linear indices of the elements in 1...n with label 'cc'.
-    // [[nodiscard]] VectorXidx mapTail(int cc, Index n) const;  //!< Get linear indices of the elements in n-1...end with label 'cc'.
-    [[nodiscard]] int label( Index i) const;     //!< Get label/index of i-th element
-    [[nodiscard]] VectorXi head( Index n) const;   //!< Get labels/indices of first n elements
-    [[nodiscard]] VectorXi tail( Index n) const;   //!< Get labels/indices of last n elements
-
-    //! Get number of connected components
-    [[nodiscard]] int number() const {  return m_comp.size()>0 ? m_comp.maxCoeff()+1 : 0; }
-
-private:
-    void dfs( const SparseMatrix<int,ColMajor> & CC,
-              int c, Index v);
-
-    VectorXi m_comp;                     // index vector components, 0-based
-    Matrix<bool,Dynamic,1> m_visited;    // for each vertex: visited?
-};*/
 
 } // namespace Graph
 
